c95ab8999ef411bc96ad6c8e8fa39a61bcd086ce
Kayke.Dev <kaykevieira.dev@gmail.com>
2026-08-29 14:56:08 -0300

    Update AcoesDosPersonagens.java

--- src/SistemaDePersonagens/AcoesDosPersonagens.java
+++ src/SistemaDePersonagens/AcoesDosPersonagens.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+package SistemaDePersonagens;
 
 public class AcoesDosPersonagens {
+    public static void main(String[] args) {
+
+        Guerreiro guerreiros = new Guerreiro("kayke", 100);
+
+        guerreiros.Status();
+        guerreiros.receberDano(30);
+        guerreiros.Status();
+        guerreiros.receberCura(20);
+        guerreiros.Status();
+        guerreiros.Ganharxp(100);
+        guerreiros.Status();
+
+        Mago mago = new Mago("Merlim");
+
+        mago.Status();
+        mago.receberDano(40);
+        mago.receberCura(20);
+        mago.habilidades();
+        mago.getMana();
+        mago.recuperarMana(20);
+        mago.habilidades();
+    }
 }
