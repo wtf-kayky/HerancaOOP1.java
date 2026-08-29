bd5fc1378c34e4b6e6fcfddddae60cc787963f4d
Kayke.Dev <kaykevieira.dev@gmail.com>
2026-08-29 14:55:51 -0300

    Update Guerreiro.java

--- src/SistemaDePersonagens/Guerreiro.java
+++ src/SistemaDePersonagens/Guerreiro.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+package SistemaDePersonagens;
+public  class Guerreiro  extends Personagem{
 
-public class Guerreiro {
+    @Override
+    public void habilidades() {
+        System.out.println("O seu guerreiro usou a habilidade dele ");
+    }
+    private int forca;
+
+    public Guerreiro(String nome, int forca) {
+        super(nome);
+        this.forca = forca;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+    public void forcaPropria(int poder){
+        this.forca = this.forca + poder;
+        if (this.forca <= 100){
+            System.out.println(" Força atual " + this.forca);
+        }else{
+            this.forca = 100;
+            System.out.println(" força atual " + this.forca);
+        }
+    }
 }
